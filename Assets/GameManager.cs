62c17b19b880c8133da590e1591c32aaff6f1707
MariusG <mg@example.com>
2018-10-06 15:41:19 +0200

    upgraded unity; added death mechanic and start menu; added buttons for controls

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
@@ -12,17 +14,64 @@ public class GameManager : MonoBehaviour {
     public Transform borderLeft;
     public Transform borderRight;
 
+    public delegate void Move(int playerIndex);
+    public static event Move MoveLeft;
+    public static event Move MoveRight;
+
+    public Button btnRightP1;
+    public Button btnLeftP1;
+
     // offset from border so that food does not spawn inside the border object
     private float offset = 0.2f;
+
+    void OnEnable()
+    {
+        Player.OnDeath += GoToMenu;
+    }
+
+    void OnDisable()
+    {
+        Player.OnDeath += GoToMenu;        
+    }
 
     // Use this for initialization
     void Start () {
         // Generate food every 5s starting in 5s
         InvokeRepeating("GenerateFood", 5.0f, 5.0f);
     }
-	
-	// Generate food at a random position on the board
-	void GenerateFood() {
+
+    public void MoveRightP1()
+    {
+        MoveRight(1);
+    }
+
+    public void MoveLeftP1()
+    {
+        MoveLeft(1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveRight(1);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveLeft(1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveRight(2);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            MoveLeft(2);
+        }
+    }
+
+    // Generate food at a random position on the board
+    void GenerateFood() {
         // x position between left & right border
         int x = (int)Random.Range(borderLeft.position.x + offset,
                                   borderRight.position.x - offset);
@@ -36,4 +85,10 @@ public class GameManager : MonoBehaviour {
                     new Vector3(x, 0.5f, z),
                     Quaternion.identity); // default rotation
     }
+
+    void GoToMenu()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
 }
+
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour {
     public float speed = 5; 
     public float rotationSpeed; // the angular speed with which the snake will turn
 
+    public int playerIndex = 1;
+
     public GameObject head; // the head GO
     public GameObject tail; // the tail GO
 
@@ -23,15 +25,50 @@ public class Player : MonoBehaviour {
     public float maxDist = 0.3f;
 
     private bool ate = false;
- 
+    private bool rotateLeft = false;
+    private bool rotateRight = false;
+
+    public delegate void deathAction();
+    public static event deathAction OnDeath;
+
+    void OnEnable()
+    {
+        GameManager.MoveLeft += MoveLeft;
+        GameManager.MoveRight += MoveRight;
+    }
+
+    void OnDisable()
+    {
+        GameManager.MoveLeft -= MoveLeft;
+        GameManager.MoveRight -= MoveRight;
+    }
+
+    void MoveLeft(int player)
+    {
+        if (player == playerIndex)
+        {
+            rotateLeft = true;
+        }
+    }
+
+    void MoveRight(int player)
+    {
+        if (player == playerIndex)
+        {
+            rotateRight = true;
+        }
+    }
+
     void Start()
     {
         body = new List<GameObject>();
         int i = 0;
         // add an initial number of body parts 
-        while (i++ < 5)
+        while (i++ < 2)
         {
             GameObject instance = Instantiate(tail, head.GetComponent<Transform>().position, Quaternion.identity) as GameObject;
+            // disable collider for all the parts at first (they are all together initially)
+            instance.GetComponent<Collider>().enabled = false;
             body.Add(instance);
         }
         // move the snek every 300ms
@@ -41,7 +78,10 @@ public class Player : MonoBehaviour {
     // moves the head of the snake and teleports the last body piece in its previous position
     void Move()
     {
-        float rotate = Input.GetAxis("Horizontal");
+        // float rotate = Input.GetAxis("Horizontal");
+        float rotate = rotateRight ? 1 : rotateLeft ? -1 : 0;
+        rotateRight = false;
+        rotateLeft = false;
 
         // get current position of the head
         Vector3 headPosition = head.GetComponent<Transform>().position;
@@ -50,6 +90,8 @@ public class Player : MonoBehaviour {
         {
             // if food has been eaten after last movement then we add a new body part instead of moving the last element
             GameObject newBody = Instantiate(tail, headPosition, Quaternion.identity) as GameObject;
+            // disable collider for the closest part
+            newBody.GetComponent<Collider>().enabled = false;
             body.Insert(0, newBody);
             ate = false;
 
@@ -60,12 +102,20 @@ public class Player : MonoBehaviour {
         {
             // Move last Tail Element to where the Head was            
             body.Last().GetComponent<Transform>().position = headPosition;
+            // disable collider for the closest part
+            body.Last().GetComponent<Collider>().enabled = false;
 
             // Add to front of list, remove from the back
             body.Insert(0, body.Last());
             body.RemoveAt(body.Count - 1);
         }
 
+        // re-enable collider for the 3rd closest body part (if there are at least 3 parts)
+        if (body.Count > 2)
+        {
+            body.ElementAt(2).GetComponent<Collider>().enabled = true;
+        }
+        
         // rotate head if key was pressed
         head.GetComponent<Transform>().Rotate(new Vector3(0, rotate * rotationSpeed, 0));
         // move head forwards (based on rotation)
@@ -104,20 +154,14 @@ public class Player : MonoBehaviour {
             // Remove the Food
             Destroy(coll.gameObject);
         }
-        // Collided with Tail or Border or other Snakes
+        // Collided with Tail or other Snakes
         else
         {
-            // ToDo 'You lose' screen
-            Debug.Log("Dead...");
-            if (body.Count > 7)
+            Debug.Log("Dead?");
+            // call the OnDeath event which will end the current game and go to the restart menu
+            if (OnDeath != null)
             {
-                int i = 0;
-                while (i++ < 5)
-                {
-                    Destroy(body.Last());
-                    body.RemoveAt(body.Count - 1);
-
-                }
+                OnDeath();
             }
         }
     }
